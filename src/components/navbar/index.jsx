15ea76854c30294336bc2961b708a6c0d77aad2d
Simon <diababdulwahed@gmail.com>
2024-04-13 21:10:13 +0200

    changes

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,13 +3,6 @@
 import React from 'react'
 import './index.css' 
 
-function App() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-
-  const [editTodoId, setEditTodoId] = useState()
-
-  const [saveEditTodo, setSaveEditTodo] = useState()
 
 
 
